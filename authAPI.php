6b956b16814712ae37cb63dad67ead1152eba72d
End Spiegel <56965868+endspigel@users.noreply.github.com>
2023-03-31 22:17:20 +0200

    Commit final

--- authAPI.php
+++ authAPI.php
@@ -3,96 +3,53 @@
 // Inclure la bibliothèque pour générer les JWT
 require('jwt_utils.php');
 
-function isValidUser($username, $password){
-    // Connexion à la base de données MySQL avec PDO
-    $pdo = new PDO('mysql:host=localhost;dbname=api-auth;=utf8mb4', 'root', '');
+// Connexion à la base de données MySQL avec PDO
+$pdo = new PDO('mysql:host=localhost;dbname=api-auth;charset=utf8mb4', 'root', '');
 
-    // Affiche les erreurs lors de l'exécution des requêtes SQL
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Affiche les erreurs lors de l'exécution des requêtes SQL
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Vérifie les identifiants envoyés via POST et renvoie un jeton JWT si les identifiants sont valides
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $posted_data = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+    $login = ($posted_data['login']);
+    $mdp = ($posted_data['mdp']);
 
     // Préparation de la requête SQL pour récupérer l'utilisateur avec le nom d'utilisateur fourni
-    $stmt = $pdo->prepare('SELECT * FROM utilisateur WHERE login = :username');
-    $stmt->bindParam(':username', $username);
-    $stmt->execute();
+    $stmt = $pdo->prepare('SELECT * FROM Utilisateur WHERE login = :login');
+
+    $stmt->execute(array('login' => $login));
 
     // Récupération de l'utilisateur correspondant
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // var_dump($user);
     // Vérification si l'utilisateur existe et si le mot de passe fourni correspond au mot de passe stocké en base de données
-    if ($user && $password === $user['mdp']) {
-        return true;
-    } else {
-        return false;
-    }
-}
-// Vérifie les identifiants envoyés via POST et renvoie un jeton JWT si les identifiants sont valides
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login']) && isset($_POST['mdp'])) {
-    $login = $_POST['login'];
-    $mdp = $_POST['mdp'];
+    if (!empty($user) && $mdp === $user['mdp']) {
+        // Récupération du rôle de l'utilisateur
+        $stmt = $pdo->prepare('SELECT nom FROM Role WHERE id = :id');
+        $stmt->execute(array('id' => $user['id']));
+        $role = $stmt->fetch(PDO::FETCH_ASSOC)['nom'];
     
-    if (isValidUser($login, $mdp)){
-        // Vérification du rôle de l'utilisateur
-        $user['role'] = $_SESSION['role'];
-
+        // Génération du jeton JWT
         $jwt_headers = array('typ' => 'JWT', 'alg' => 'HS256');
-        $jwt_payload = array('login' => $login, 'exp' => time() + 60); // expire dans une minute
+        $jwt_payload = array('login' => $login, 'role' => $role, 'exp' => time() + 3600); // expire dans une heure
         $jwt = generate_jwt($jwt_headers, $jwt_payload);
-
+    
+        // Envoi de la réponse avec le jeton JWT
+        http_response_code(200);
         header('Content-Type: application/json');
-        //echo json_encode(array('token' => $jwt));
-        echo "Connexion réussie";
-        header('Location: serveurREST.php');
+        echo json_encode(array('jwt' => $jwt));
+        
     } else {
+        // Envoi d'une réponse d'erreur si les identifiants sont invalides
         http_response_code(401); // Non autorisé
-        echo "Nom d'utilisateur ou mot de passe incorrect.";
+        echo json_encode(array('message' => 'Identifiants invalides.'));
     }
-
-    /*if (isValidUser($login, $mdp)){
-        echo "Connexion réussie !";
-    } else {
-        http_response_code(401); // Non autorisé
-        echo "Nom d'utilisateur ou mot de passe incorrect.";
-    }*/
-}
-
-// Vérifie si un jeton JWT est valide
-if (isset($_GET['token']) && is_jwt_valid($_GET['token'])){
-    http_response_code(200);
-    echo "Le jeton est valide.";
+    
 } else {
-
-    // Affiche la page de connexion si le formulaire n'a pas été envoyé ou si le jeton est invalide
-    ?>
-    <!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Page de connexion</title>
-    </head>
-    <body>
-        <h1>Page de connexion</h1>
-        <form method="post" action="authAPI.php">
-            <label for="username">Nom d'utilisateur :</label>
-            <input type="text" id="login" name="login"><br><br>
-            <label for="password">Mot de passe :</label>
-            <input type="password" id="mdp" name="mdp"><br><br>
-            <input type="submit" value="Se connecter">
-        </form>
-    </body>
-    </html>
-
-<?php  }
-
-$database = new PDO('mysql:host=localhost;dbname=api-auth;=utf8mb4', 'root', '');
-
-$requete = $database->prepare('SELECT contenu FROM article');
-$requete->execute();
-
-while($result = $requete->fetch()):
-    echo $result['contenu'];
-
-endwhile;
-
+    // Envoi d'une réponse d'erreur pour les requêtes autres que POST
+    http_response_code(405); // Méthode non autorisée
+    echo json_encode(array('message' => 'Méthode non autorisée.'));
+}
 ?>
-
--- serveurREST.php
+++ serveurREST.php
@@ -1,113 +1,107 @@
 <?php
 require('jwt_utils.php');
-
+require('api_utils.php');
 $database = new PDO('mysql:host=localhost;dbname=api-auth;=utf8mb4', 'root', '');
 
 $requete = $database->prepare('SELECT date_publication, contenu, login FROM article');
 $requete->execute();
 
+$jwt = '';
 $jwt = get_bearer_token(); // fonction qui extrait le jeton JWT de la requête
 
-// Vérification de l'authentification
-if (is_jwt_valid($jwt)) {
-    http_response_code(401); // Non autorisé
-    echo json_encode(array("message" => "Authentification requise pour accéder à cette page."));
-    exit();
+// Récupération de la méthode HTTP utilisée
+$http_method = $_SERVER['REQUEST_METHOD'];
+
+if($jwt == NULL){
+    procedureAnonymous($http_method);
+} else {
+    if (!is_jwt_valid($jwt)) {
+        http_response_code(401); // Non autorisé
+        echo json_encode(array("message" => "Authentification requise pour accéder à cette page."));
+        exit();
+    }
+    $role = getRole($login);
+    switch($role){
+        case('Moderator'):
+            procedureRoleModerator($http_method);
+            break;
+        case('Publisher'):
+            procedureRolePublisher($http_method);
+        default:
+        deliver_response(400, "Rôle invalide", null);
+    }
+}
+
+function procedureRoleModerator($http_method) {
+    switch($http_method){
+        case "GET":
+            if(!empty($_GET['id_article'])){
+                $reponse = getById($id);
+            } else {
+                $response = getAllDataForModePubli();
+            }
+            deliver_response(200, "Votre message", $reponse);
+            break;
+        case "DELETE":
+            if(!empty($_GET['id_article'])){
+                $id = htmlspecialchars($_GET['id_article']);
+                $response = deletePhrase($id);
+                deliver_response(204, "Article bien supprimé", $reponse);
+            } else {
+                deliver_response(400, "L'identifiant de l'article est manquant", $reponse);
+            }
+            break;
+        default:
+            http_response_code(405); // Method Not Allowed
+            echo json_encode(array("message" => "Méthode HTTP non autorisée."));
+    }
+}
+
+function procedureRolePublisher($http_method) {
+    switch($http_method){
+        case "GET":
+            if(!empty($_GET['id_article'])){
+                $reponse = getById($id);
+            } else {
+                $response = getAllDataForModePubli();
+            }
+            deliver_response(200, "Votre message", $reponse);
+            break;
+        case "POST":
+            $json = file_get_contents('php://input');
+            $article = json_decode($json, true);
+            $contenu = $article['contenu'];
+            if (!empty($contenu)){
+                $reponse=postNewArticle($contenu);
+            }
+            break;
+        case "PUT":
+            if(!empty($_GET['id_article'])){
+                $id = htmlspecialchars($_GET['id_article']);
+                $json = file_get_contents('php://input');
+                $article = json_decode($json, true);
+                $contenu = $article['contenu'];
+                $reponse = putPhrase($id, $contenu);
+                deliver_response(200, "Votre message", $reponse);
+            } else {
+                echo json_encode(array("message" => "L'identifiant de l'article est manquant."));
+            }
+            break;
+        case "DELETE":
+            if(!empty($_GET['id_article'])){
+                $id = htmlspecialchars($_GET['id_article']);
+                $reponse = deletePhrase($id);
+                deliver_response(204, "La phrase a bien été supprimé", $response);
+            } else {
+                deliver_response(400, "L'identifiant de l'article est manquant.", null);
+            }
+            break;
+        default:
+            http_response_code(405); // Method Not Allowed
+            echo json_encode(array("message" => "Méthode HTTP non autorisée."));
+    }
 }
 
 // Récupération du login de l'utilisateur
 $jwt_payload = (array) jwt_decode($jwt);
 $login = $jwt_payload['login'];
-
-// Récupération du rôle de l'utilisateur
-$stmt = $database->prepare('SELECT nom FROM Role WHERE id = (SELECT id FROM Utilisateur WHERE login = :login)');
-$stmt->bindParam(':login', $login);
-$stmt->execute();
-$user_role = $stmt->fetchColumn();
-
-// Récupération de la méthode HTTP utilisée
-$http_method = $_SERVER['REQUEST_METHOD'];
-
-// Vérification du rôle de l'utilisateur et traitement en conséquence
-switch($user_role){
-    case('Moderator'):
-        // GET tous les articles
-        // GET informations sur un article
-        // DELETE : supprimer n'importe quel article
-        switch ($http_method){
-            case "GET":
-                if(!empty($_GET['id_article'])){
-                    $reponse = getById($id);
-                } else {
-                    $response = getAllDataForModePubli();
-                }
-                deliver_response(200, "Votre message", $reponse);
-                break;
-            case "DELETE":
-                if(!empty($_GET['id_article'])){
-                    $id = htmlspecialchars($_GET['id_article']);
-                    $response = deletePhrase($id);
-                    deliver_response(204, "Article bien supprimé", $reponse);
-                } else {
-                    deliver_response(400, "L'identifiant de l'article est manquant", $reponse);
-                }
-                break;
-            default:
-                http_response_code(405); // Method Not Allowed
-                echo json_encode(array("message" => "Méthode HTTP non autorisée."));
-        }
-        break;
-    case('Publisher'):
-        // AJOUTER LE GET POUR QU4IL VOIT SES ARTICLES A LUI
-        // POST publier un nouvel article
-        // GET consulter un article
-        // GET consulter tous les articles
-        // MODIFY : modifier un des articles de l'utilisateur
-        // DELETE : supprimer un de ses articles
-        switch($http_method){
-            case "GET":
-                if(!empty($_GET['id_article'])){
-                    $reponse = getById($id);
-                } else {
-                    $response = getAllDataForModePubli();
-                }
-                deliver_response(200, "Votre message", $reponse);
-                break;
-            case "POST":
-                $json = file_get_contents('php://input');
-                $article = json_decode($json, true);
-                $contenu = $article['contenu'];
-                if (!empty($contenu)){
-                    $reponse=postNewArticle($contenu);
-                }
-                break;
-            case "PUT":
-                if(!empty($_GET['id_article'])){
-                    $id = htmlspecialchars($_GET['id_article']);
-                    $json = file_get_contents('php://input');
-                    $article = json_decode($json, true);
-                    $contenu = $article['contenu'];
-                    $reponse = putPhrase($id, $contenu);
-                    deliver_response(200, "Votre message", $reponse);
-                } else {
-                    echo json_encode(array("message" => "L'identifiant de l'article est manquant."));
-                }
-                break;
-            case "DELETE":
-                if(!empty($_GET['id_article'])){
-                    $id = htmlspecialchars($_GET['id_article']);
-                    $reponse = deletePhrase($id);
-                    deliver_response(204, "La phrase a bien été supprimé", $response);
-                } else {
-                    deliver_response(400, "L'identifiant de l'article est manquant.", null);
-                }
-                break;
-            default:
-                http_response_code(405); // Method Not Allowed
-                echo json_encode(array("message" => "Méthode HTTP non autorisée."));
-        }
-        break;
-    default:
-        http_response_code(403); // Forbidden
-        echo json_encode(array("message" => "Vous n'avez pas les droits nécessaires pour accéder à cette ressource."));
-    }
