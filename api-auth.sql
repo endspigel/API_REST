a7b7c29d7ef9c11f64fe85aef3ff805fb1a58ec9
End Spiegel <56965868+endspigel@users.noreply.github.com>
2023-03-31 23:16:00 +0200

    Update api-auth.sql

--- api-auth.sql
+++ api-auth.sql
@@ -1,21 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : ven. 31 mars 2023 à 18:53
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `api-auth`
